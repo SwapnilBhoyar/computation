11233f91ed63b746327851061bf9a13e1906d121
SwapnilBhoyar <swapnilbhoyar95@gmail.com>
2021-04-08 14:26:35 +0530

    [Swapnil] added compute.sh

--- compute.sh
+++ compute.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-declare -a result
+declare -A result
+declare -a result_array
 read -p "Enter a:" a
 read -p "Enter b:" b
 read -p "Enter c:" c
@@ -8,3 +9,7 @@ operation2=$(( a*b+c ))
 operation3=$(( a+b/c ))
 operation4=$(( a%b+c ))
 result=(["1"]=$operation1 ["2"]=$operation2 ["3"]=$operation3 ["4"]=$operation4)
+for value in ${!result[@]}
+do
+	result_array[$value]=${result[$value]}
+done
